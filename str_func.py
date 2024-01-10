36136b157682441370dcfe3a65301c5cea70a6a6
Тоторо <roman.aka.tod@gmail.com>
2024-01-10 18:30:59 +0300

    str uppercase new function added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def uppercase_str(s):
+    """blablabla"""
     return s.upper()
+
+
+def uppercase_first(s):
+    """blablabla"""
+    sl = s.split()
+    for i in sl:
+        i.capitalize()
+    return " ".join(sl)
